11755e3a9d578b6bb2ab074faa39106a6f5feb76
Jake McIvor <jake.mcivor@gmail.com>
2023-11-23 22:59:22 -0700

    Beta Header Update

--- Software/LipSyncX/LSCircularBuffer.h
+++ Software/LipSyncX/LSCircularBuffer.h
@@ -1,9 +1,21 @@
 /* 
 * File: LSCircularBuffer.h
-* Firmware: LipSync4
+* Firmware: LipSync X
 * Edited by: MakersMakingChange
-* Based on: CircularBuffer.h
-* Version: Alpha 2 (14 April 2022) 
+* Version: Beta (23 November 2023) 
+  License: GPL v3.0 or later
+
+  Copyright (C) 2023 Neil Squire Society
+  This program is free software: you can redistribute it and/or modify it under the terms of
+  the GNU General Public License as published by the Free Software Foundation,
+  either version 3 of the License, or (at your option) any later version.
+  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License along with this program.
+  If not, see <http://www.gnu.org/licenses/>
+
+  * Based on: CircularBuffer.h
 */
 //Header definition
 #ifndef _LSCIRCULARBUFFER_H
