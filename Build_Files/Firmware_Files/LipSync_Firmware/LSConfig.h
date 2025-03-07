9870a9a69d2b235e5de873df896f5e3c8207236e
jversloot <50920198+jversloot@users.noreply.github.com>
2025-03-07 12:38:52 -0400

    Updated scroll poll rate

--- Build_Files/Firmware_Files/LipSync_Firmware/LSConfig.h
+++ Build_Files/Firmware_Files/LipSync_Firmware/LSConfig.h
@@ -163,7 +163,7 @@ const String lipsyncVersionStr = String(CONF_LIPSYNC_VERSION_MAJOR) + "." + Stri
 
 // Polling rates for each module
 #define CONF_JOYSTICK_POLL_RATE 20          // 20 ms 
-#define CONF_SCROLL_POLL_RATE 20           // 140 ms
+#define CONF_SCROLL_POLL_RATE 30            // 30 ms
 #define CONF_PRESSURE_POLL_RATE 50          // 50 ms
 #define CONF_INPUT_POLL_RATE 20             // 20 ms
 #define CONF_BT_FEEDBACK_POLL_RATE 1000     // 1s         
