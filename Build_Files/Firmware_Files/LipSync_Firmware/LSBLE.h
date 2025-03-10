de69623a0c976491948c7baf32d461d47b72ad2c
Jake McIvor <jake.mcivor@gmail.com>
2025-03-10 11:16:23 -0600

    Update dates for v4.1rc

--- Build_Files/Firmware_Files/LipSync_Firmware/LSBLE.h
+++ Build_Files/Firmware_Files/LipSync_Firmware/LSBLE.h
@@ -2,7 +2,7 @@
 * File: LSBLE.h
 * Firmware: LipSync
 * Developed by: MakersMakingChange
-* Version: v4.1rc (20 January 2025)
+* Version: v4.1rc (10 March 2025)
   License: GPL v3.0 or later
 
   Copyright (C) 2024 - 2025 Neil Squire Society
